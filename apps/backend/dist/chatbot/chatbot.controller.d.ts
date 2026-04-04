c523b77aab5bd4a95103a30257d241aa72c7f87c
Virothi-Chaithanya <vchaithu25@gmail.com>
2026-04-05 01:52:29 +0530

    cleanup build files

--- apps/backend/dist/chatbot/chatbot.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class ChatbotController {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    chat(body: {
-        message: string;
-        context?: any;
-    }): Promise<any>;
-}
